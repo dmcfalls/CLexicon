8355c971cb8ba5ffe7c976212ad21380063f5d64
dmcfalls <dmcfalls@stanford.edu>
2015-10-21 16:08:35 -0700

    added CLexicon.h, CLexicon.c, and a sample dictionary file

---
+++ CLexicon.c
@@ -0,0 +1,272 @@
+/* Filename: CLexicon.c
+ * Author: Dan McFalls (dmcfalls@stanford.edu)
+ * -------------------------------------------
+ * A Lexicon is a data structure used for the efficient storage and
+ * retrieval of strings (char*). It functions the same as a set but is
+ * internally optimized to process strings efficiently. The internal
+ * structure of the CLexicon is a tree of "LexNodes" in which each LexNode
+ * has 26 children, one for each letter of the English alphabet.
+ *
+ * Modeled after the "Lexicon" class from the Stanford C++ Libraries.
+ */
+
+#include "CLexicon.h"
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define ALPHA_SIZE 26
+#define MAX_WORD_LEN 45
+
+
+            /*** struct definitions ***/
+
+
+struct LexNode {
+    bool is_word;
+    LexNode* children[ALPHA_SIZE];
+};
+
+struct CLexiconImplementation {
+    LexNode* root;
+    int wordcount;
+};
+
+
+            /*** local helper functions ***/
+
+/* Function: create_node
+ * -----------------------
+ * Creates a new LexNode, initializes all of its children pointers to NULL,
+ * and sets its word status to false. Returns a pointer.
+ */ 
+LexNode* create_node() {
+    LexNode* node  = malloc(sizeof(LexNode*) + sizeof(LexNode));
+    for(int i = 0; i < ALPHA_SIZE; i++) {
+        node->children[i] = NULL;
+    }
+    node->is_word = false;
+    return node;
+}
+
+/* Function: delete_node_helper
+ * ----------------------------
+ * Helper function for delete_node. Passes LexNode pointers-to-pointers so that
+ * the actual tree can be recursively deleted.
+ */ 
+void delete_node_helper(LexNode** nodeptr) {
+    if(*nodeptr == NULL) return;
+
+    for(int i = 0; i < ALPHA_SIZE; i++) {
+        delete_node_helper(&(*nodeptr)->children[i]);
+    }
+    free(*nodeptr);
+}
+
+/* Function: delete_node
+ * ---------------------
+ * Deletes a node and all of its children nodes and frees all memory associated with them.
+ */ 
+void delete_node(LexNode* node) {
+    if(node == NULL) return;
+
+    for(int i = 0; i < ALPHA_SIZE; i++) {
+        delete_node_helper(&node->children[i]);
+    }
+    free(node);
+}
+
+/* Fuction: clex_simple_add
+ * ------------------------
+ * Adds a word in all lower-case with length wordlen to the lexicon. Avoids checking the
+ * case of the word. Retains the "clex" prefix because it could theoretically be listed
+ * in the header file and considered available for client use. Remains hidden now for the
+ * sake of simplicity of presentation.
+ */ 
+void clex_simple_add(CLexicon* lex, char* word_lower, int wordlen) {
+    //For every character in the word, accesses (or creates) subnodes.
+    LexNode* last_node = lex->root;
+    for(int i = 0; i < wordlen; i++) {
+        char ith = word_lower[i];
+        LexNode** next_node = &last_node->children[ith - 'a'];
+        if(*next_node == NULL) {
+            *next_node = create_node();
+        }
+        last_node = *next_node;
+    }
+    //Indicates that the final node, representing the string word, is added to the lexicon.
+    last_node->is_word = true;
+    lex->wordcount++;
+}
+
+            /*** client functions listed in the header file ***/
+
+/* Function: clex_create
+ * ---------------------
+ * Creates a new CLexicon* which is initialized and returned. The client only accesses
+ * a pointer to the CLexicon and should never need to interact with the actual struct.
+ */
+CLexicon* clex_create() {
+    CLexicon* lex = malloc(sizeof(CLexicon*) + sizeof(CLexicon));
+    lex->root = create_node();
+    lex->wordcount = 0;
+    return lex;
+}
+
+/* Function: clex_delete
+ * ---------------------
+ * Frees all memory associated with a CLexicon*.
+ */ 
+void clex_delete(CLexicon* lex) {
+   delete_node(lex->root);
+   free(lex);
+}
+
+/* Function: clex_add
+ * ------------------
+ * Adds the given word to the CLexicon after converting it to lower case.
+ */ 
+void clex_add(CLexicon* lex, char* word) {
+    //printf("adding word: %s\n", word);
+    int wordlen = strlen(word);
+    //Creates a lower-case copy of the word.
+    char word_lower[wordlen+1];
+    for(int i = 0; i < wordlen; i++) {
+        word_lower[i] = tolower(word[i]);
+    }
+    word_lower[wordlen] = '\0';
+
+    //simple_add is a helper function that adds the word and increments the wordcount.
+    clex_simple_add(lex, word_lower, wordlen);
+}
+
+/* Function: clex_add_from_file
+ * ----------------------------
+ * This function fills a lexicon with entires from a text file where each line
+ * contains a single string of text with letters a-z or A-Z. If the file cannot
+ * be opened or a bad line of text is reached, the function returns false to indicate
+ * a failure in reading. If "is_lower_case" is true, uses clex_simple_add to add each
+ * line, which does not check case. If it is false, uses clex_add, which is more expensive.
+ */ 
+bool clex_add_from_file(CLexicon* lex, char* filename, bool is_lower_case) {
+    FILE* lex_file = fopen(filename, "r");
+    if(lex_file == NULL) {
+        return false;
+    }
+
+    char line[MAX_WORD_LEN+1];
+    //Reads lines from the file, adding them to the lexicon one by one.
+    while(fgets(line, sizeof(line), lex_file)) {
+        //Scans the line to make sure that it was processed correctly. 45 is MAX_WORD_LEN.
+        char word[MAX_WORD_LEN+1];
+        int nscanned = sscanf(line, "%45[ a-zA-Z]", word);
+        if(nscanned != 1) {
+            fclose(lex_file);
+            return false;
+        }
+
+        //Adds the word depending on whether it is guarunteed to be lower case or not.
+        is_lower_case ? clex_simple_add(lex, word, strlen(word)) : clex_add(lex, word);
+    }
+
+    fclose(lex_file);
+    return true; 
+}
+
+/* Function: clex_clear
+ * --------------------
+ * Deletes all entires from the CLexicon, initializes a new root node, and sets
+ * the word count to zero.
+ */ 
+void clex_clear(CLexicon* lex) {
+    delete_node(lex->root);
+    lex->root = create_node();
+    lex->wordcount = 0;
+}
+
+/* Function: clex_contains
+ * -----------------------
+ * Traverses the tree from the root until it reaches the node that corresponds
+ * to the given word. Returns that node's is_word field, indicating whether word
+ * is a member of the CLexicon.
+ */ 
+bool clex_contains(CLexicon* lex, char* word) {
+    //Creates a lower-case copy of the word.
+    int wordlen = strlen(word);
+    char word_lower[wordlen+1];
+    for(int i = 0; i < wordlen; i++) {
+        word_lower[i] = tolower(word[i]);
+    }
+    word_lower[wordlen] = '\0';
+
+    //Traverses the tree from the root until it reaches the target node.
+    LexNode* curr_node = lex->root;
+    for(int i = 0; i < wordlen; i++) {
+        //If the tree ends before target node is reached, simply returns false.
+        if(curr_node == NULL) return false;
+        char ith = word_lower[i];
+        curr_node = curr_node->children[ith - 'a'];
+    }
+    
+    if(curr_node == NULL) return false;
+    return curr_node->is_word;
+}
+
+/* Function: clex_isEmpty
+ * ----------------------
+ * Returns true if the CLexicon is empty, false otherwise.
+ * Named in lowerCamelCase because every isEmpty function I've ever encountered
+ * has looked that way, and it just seems to fit better than is_empty.
+ */ 
+bool clex_isEmpty(CLexicon* lex) {
+    return lex->wordcount == 0;
+}
+
+/* Function: clex_remove
+ * ---------------------
+ * Removes the target word from the CLexicon by traversing the tree until it reaches
+ * the node corresponding to word, then changing its is_word field to false. If there
+ * are no nodes beneath the word node, deletes the node from the tree and sets its
+ * parent's pointer to it to NULL. Returns false if the word is not a member of the CLexicon.
+ */ 
+bool clex_remove(CLexicon* lex, char* word) {
+    //Creates a lower-case copy of word.
+    int wordlen = strlen(word);
+    char word_lower[wordlen+1];
+    for(int i = 0; i < wordlen; i++) {
+        word_lower[i] = tolower(word[i]);
+    }
+    word_lower[wordlen] = '\0';
+
+    //Iterates through the letters, descending down the tree to the penultimate letter.
+    LexNode** curr_node = &lex->root;
+    for(int i = 0; i < wordlen - 1; i++) {
+        if(*curr_node == NULL) return false;
+        char ith = word_lower[i];
+        //printf("remove: examining letter %c\n", ith);
+        curr_node = &((*curr_node)->children[ith - 'a']);
+    }
+    LexNode** word_node = &(*curr_node)->children[word_lower[wordlen - 1] - 'a'];
+    //printf("remove: examining letter %c\n", word_lower[wordlen - 1]);
+    if(*word_node == NULL) return false;
+    (*word_node)->is_word = false;
+    lex->wordcount--;
+
+    //Checks to see if word_node has children. If not, deletes the node and points curr_node to NULL.
+    for(int i = 0; i < ALPHA_SIZE; i++) {
+        if((*word_node)->children[i] != NULL) return true;
+    }
+    free(*word_node);
+    *word_node = NULL;
+    return true;
+}
+
+/* Function: clex_wordcount
+ * ------------------------
+ * Returns the number of entries in the CLexicon.
+ */ 
+int clex_wordcount(CLexicon* lex) {
+    return lex->wordcount;
+}
---
+++ CLexicon.h
@@ -0,0 +1,123 @@
+/* File: CLexicon.h
+ * Author: Dan McFalls (dmcfalls@stanford.edu)
+ * -------------------------------------------
+ * CLexicon is a data structure created for the efficient storage
+ * and retrieval of words (strings). It is equivalent to a set but
+ * is internally optimized to process strings very efficiently.
+ *
+ * Modeled after "Lexicon" from the Stanford C++ Libraries.
+ */
+
+#ifndef _CLexicon_h
+#define _CLexicon_h
+
+#include <stdbool.h>    //defines the bool type
+
+
+            /*** struct partial definitions ***/
+
+
+/* Struct: LexNode
+ * ---------------
+ * Incomplete declaration of struct LexNode. LexNode is the basic unit of the CLexicon,
+ * and is used to create the tree structure with hightened efficiency for word processing.
+ * Its specifications remain opaque in the header file.
+ */ 
+typedef struct LexNode LexNode;
+
+/* Struct: CLexiconImplementation
+ * ------------------------------
+ * Incomplete declaration of struct CLexiconImplementation. The actual implementation of the
+ * CLexicon occurs in the C file to keep the header file clean and straightforward.
+ */ 
+typedef struct CLexiconImplementation CLexicon;
+
+
+            /*** "public" methods intended for client use ***/
+
+
+/* Function: clex_create
+ * ---------------------
+ * Creates a pointer to a CLexicon struct and initializes all of its fields.
+ * The client accesses only these external methods and does not worry about the
+ * internal structure of the CLexicon. This prevents tampering that might cause
+ * certain methods to stop functioning correctly.
+ * Runs in constant time.
+ */ 
+CLexicon* clex_create();
+
+
+/* Function: clex_delete
+ * ---------------------
+ * Deletes the CLexicon and frees all memory associated with it.
+ * Runs in linear time (scaling with the size of the lexicon).
+ */ 
+void clex_delete(CLexicon* lex);
+
+
+/* Function: clex_add
+ * ------------------
+ * Adds the given string word to the CLexicon. This operation begins (relatively) slowly
+ * when the first words are being added and increases in efficiency as the lexicon grows larger.
+ * This is possible because the lexicon is structured specifically to work with strings.
+ * Runs in linear time (scaling with the length of word).
+ */
+void clex_add(CLexicon* lex, char* word);
+
+
+/* Function: clex_add_from_file
+ * ----------------------------
+ * Adds words from a file with the given name to the CLexicon. The file must be formatted such that
+ * a single word appears on each line. Words should not exceed the maxmimum length of the longest
+ * English word included in a major dictionary (pneumonoultramicroscopicsilicovalcanoconiosis).
+ * The final boolean argument indicates whether the words read from the file are guaranteed to be
+ * in lower case. If this is true, the operation for adding each word will be more efficient.
+ * Runs in quadratic time (scaling with file size and lengths of words).
+ */ 
+bool clex_add_from_file(CLexicon* lex, char* filename, bool is_lower_case);
+
+
+/* Function: clex_clear
+ * --------------------
+ * Clears all elements from the CLexicon. Distinct from clex_delete in that it does not
+ * free the memory associated with the data structure, and leaves it able to be used for further
+ * adding, searching, and removing operations.
+ */ 
+void clex_clear(CLexicon* lex);
+
+
+/* Function: clex_contains
+ * -----------------------
+ * Searches the CLexicon for a given word and returns true if it contains it, otherwise returning
+ * false. The internal structure of the lexicon ensures that this operation is significantly faster
+ * than running a binary search on a sorted set of strings.
+ * Runs in linear time (scaling with the length of the word).
+ */ 
+bool clex_contains(CLexicon* lex, char* word);
+
+
+/* Function: clex_isEmpty
+ * ----------------------
+ * Returns true if the given CLexicon is empty, false otherwise.
+ * Runs in constant time.
+ */ 
+bool clex_isEmpty(CLexicon* lex);
+
+
+/* Function: clex_remove
+ * ---------------------
+ * Removes the given word from the CLexicon, returning true on a successful removal.
+ * If the word is not found in the CLexicon, returns false.
+ * Runs in linear time (scaling with the length of the word).
+ */ 
+bool clex_remove(CLexicon* lex, char* word);
+
+
+/* Function: clex_word_count
+ * -------------------------
+ * Returns the number of elements currently stored in the CLexicon.
+ * Runs in constant time.
+ */ 
+int clex_wordcount(CLexicon* lex);
+
+#endif
